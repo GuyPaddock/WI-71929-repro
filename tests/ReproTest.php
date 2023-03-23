f8f48c959d140e5af00ceb3e56c3de7e8872e5cd
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2023-03-23 16:20:59 -0400

    Initial Commit

---
+++ tests/ReproTest.php
@@ -0,0 +1,13 @@
+<?php /** @noinspection ALL */
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ReproTest extends KernelTestCase {
+
+  public function testRepro(): void {
+    $this->assertTrue(true, 'This just ensures this test passes.');
+  }
+
+}
